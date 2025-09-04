ad99928f2362a3e676169148516e4c4be8509635
Lukasz Kmiecik <kmieciklu@gmail.com>
2025-09-04 11:34:13 +0200

    new

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     ul = {}
 
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
         if key in ul:
             print("Getting from cache")
